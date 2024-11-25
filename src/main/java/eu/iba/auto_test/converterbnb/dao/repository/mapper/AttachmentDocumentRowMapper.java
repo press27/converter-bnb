e35d776e3b982fc3db7fd9cf30909e31ad9a5891
PetrikAleksey <47816260+PetrikAleksey@users.noreply.github.com>
2024-11-25 09:25:29 +0300

    fix Attachment map

--- src/main/java/eu/iba/auto_test/converterbnb/dao/repository/mapper/AttachmentDocumentRowMapper.java
+++ src/main/java/eu/iba/auto_test/converterbnb/dao/repository/mapper/AttachmentDocumentRowMapper.java
@@ -2,13 +2,19 @@ package eu.iba.auto_test.converterbnb.dao.repository.mapper;
 
 import eu.iba.auto_test.converterbnb.dao.model.AttachmentDocument;
 import eu.iba.auto_test.converterbnb.dao.model.Author;
+import eu.iba.auto_test.converterbnb.utils.AttachmentUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.util.StreamUtils;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 
 public class AttachmentDocumentRowMapper implements RowMapper<AttachmentDocument> {
+
+    public static final Logger log = LoggerFactory.getLogger(AttachmentDocumentRowMapper.class);
 
     @Override
     public AttachmentDocument mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -36,11 +42,26 @@ public class AttachmentDocumentRowMapper implements RowMapper<AttachmentDocument
             model.setName(rs.getString("name"));
             model.setContentType(rs.getString("contentType"));
             model.setSize(rs.getLong("size"));
-            model.setData(rs.getString("data"));
+            model.setData(getSafeHex(rs,"data"));
             model.setCrc(rs.getString("crc"));
             return model;
         } catch (Exception e) {
             throw new SQLException(e);
         }
     }
+
+    private String getSafeHex(ResultSet rs, String column) {
+        try {
+            return AttachmentUtils.bytesToHex(rs.getBytes(column));
+        } catch (Exception ex){
+            log.error(ex.getMessage(),ex);
+            try {
+                return AttachmentUtils.bytesToHex(StreamUtils.copyToByteArray(rs.getBlob(column).getBinaryStream()));
+            } catch (Exception ex2){
+                log.error(ex2.getMessage(),ex2);
+            }
+            return null;
+        }
+    }
+
 }
---
+++ src/main/java/eu/iba/auto_test/converterbnb/utils/AttachmentUtils.java
@@ -0,0 +1,21 @@
+package eu.iba.auto_test.converterbnb.utils;
+
+public class AttachmentUtils {
+
+
+    public static String bytesToHex(byte[] bytes) {
+        if (bytes == null || bytes.length == 0){
+            return null;
+        }
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+}
